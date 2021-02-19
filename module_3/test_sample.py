79f600038cdbb09b7d500cd35042d757e694bc55
st1gm4t4 <tkilla.msk@gmail.com>
2021-02-19 11:03:42 +0300

    fixed test_sample.py after review

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -24,28 +24,20 @@ email_address = f'test+{uuid.uuid4().hex}@test.com'
 
 
 def test_add_to_basket_available_item():
-    # Arrange
     browser = webdriver.Chrome()
     browser.get(available_product_url)
-    # Берем название и цену для последующего сравнения с текстом в плашках
     item_price = browser.find_element(By.CSS_SELECTOR, item_price_locator).text
     item_name = browser.find_element(By.CSS_SELECTOR, item_name_locator).text
 
-    # Act
     try:
-        # Проверка доступности товара для заказа по наличию элемента "на складе", ждем загрузку страницы
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, instock_availability_locator))
         )
-        # Добавление в корзину
         browser.find_element(By.CSS_SELECTOR, add_to_basket_button_locator).click()
 
-    # Assert
-        # Проверка стоимости товара в плашке "Стоимость корзины теперь составляет"
         EC.presence_of_element_located((By.CSS_SELECTOR, instock_availability_locator))
         assert item_price == browser.find_element(By.CSS_SELECTOR, basket_total_sum_popup_locator).text, \
             "Item price in popup didn't match actual price"
-        # Проверка названия товара в плашке "... был добавлен в вашу корзину"
         assert item_name == browser.find_element(By.CSS_SELECTOR, item_name_popup_locator).text, \
             "Item name in popup didn't match actual item name"
 
@@ -55,21 +47,19 @@ def test_add_to_basket_available_item():
 
 
 def test_add_to_basket_unavailable_item_and_check_notification():
-    # Arrange
     browser = webdriver.Chrome()
     browser.get(unavailable_product_url)
 
-    # Act
     try:
-        # Количество товара на складе не показывается, ждем загрузку страницы
+
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, out_of_stock_availability_locator))
         )
-        # Проверяем отсутствие кнопки "Добавить в корзину"
+
         add_to_basket_button_list = browser.find_elements(By.CSS_SELECTOR, add_to_basket_button_locator)
         assert len(add_to_basket_button_list) == 0, \
             "Кнопка 'Добавить в корзину' есть на странице"
-        # Проверяем работу кнопки "Сообщить мне"
+
         browser.find_element(By.CSS_SELECTOR, email).send_keys(email_address)
         browser.find_element(By.CSS_SELECTOR, notify_me_button_locator).click()
         WebDriverWait(browser, 10).until(
