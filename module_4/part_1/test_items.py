cd1c4716a1cf567152e6b16dcd0ea2428a341d55
st1gm4t4 <tkilla.msk@gmail.com>
2021-03-04 03:40:39 +0300

    tasks for module 4

---
+++ module_4/part_1/test_items.py
@@ -0,0 +1,16 @@
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+add_to_basket_button_locator = '#add_to_basket_form > button'
+# надо добавить остальные языки
+button_names = {
+    'ru' : 'Добавить в корзину',
+    'en-gb' : 'Add to basket',
+    'de' : 'In Warenkorb legen',
+    'da' : 'Læg i kurv',
+    'es' : 'Añadir al carrito'
+}
+
+
+def test_button_language(browser, browser_lang):
+    browser.get(link)
+    assert browser.find_element_by_css_selector(add_to_basket_button_locator).text == \
+           button_names[browser_lang], 'Language mismatch'
---
+++ module_4/part_2/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser = None
+    options = Options()
+    options.add_argument("--start-maximized")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
