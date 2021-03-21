fb6a9fe55404984006b95fa0b57c84ed0f8244d2
st1gm4t4 <tkilla.msk@gmail.com>
2021-03-21 17:57:30 +0300

    fixed Allure result and report

---
+++ final/pages/locators.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, "div.basket-mini.pull-right.hidden-xs > span > a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+    SEARCH_FIELD = (By.CSS_SELECTOR, "#id_q")
+    SEARCH_BUTTON = (By.CSS_SELECTOR, "div.navbar-collapse.primary-collapse.collapse > form > input")
+    SEARCH_RESULT = (By.CSS_SELECTOR, "article > h3 > a")
+    NOTHING_FOUND = (By.CSS_SELECTOR, "#default > div.container-fluid.page > div > div > div > form > p")
+
+
+class BasketPageLocators():
+    BASKET_IS_EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
+    ITEMS_IN_BASKET = (By.CSS_SELECTOR, "#basket_formset > div")
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REG_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL_REG = (By.CSS_SELECTOR, "#id_registration-email")
+    PASS_REG = (By.CSS_SELECTOR, "#id_registration-password1")
+    CONFIRM_PASS_REG = (By.CSS_SELECTOR, "#id_registration-password2")
+    REG_BTN = (By.CSS_SELECTOR, "#register_form > button")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div.alert.alert-success.fade.in > div")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main > h1")
+    PRODUCT_NAME_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    TOTAL_PRICE = (By.CSS_SELECTOR, "div.basket-mini.pull-right.hidden-xs")
+    TOTAL_PRICE_MESSAGE = (
+    By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
---
+++ final/test_product_page.py
@@ -0,0 +1,138 @@
+import uuid
+
+import pytest
+import time
+
+from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+
+
+class TestProductPage():
+    @pytest.mark.parametrize('promo_offer',
+                             ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6",
+                              pytest.param("offer7", marks=pytest.mark.xfail), "offer8",
+                              "offer9"])
+    def test_guest_can_add_product_to_basket(self, browser, promo_offer):
+        browser.delete_all_cookies()
+        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={promo_offer}"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_be_alert()
+        page.solve_quiz_and_get_code()
+        page.should_be_success_message()
+        page.should_change_price()
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        page.should_be_login_page()
+
+    @pytest.mark.xfail
+    def test_message_disappeared_after_adding_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_be_disappeared_success_message()
+
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        basket_page = BasketPage(browser, link)
+        basket_page.go_to_basket_page()
+        basket_page.should_be_empty_basket()
+
+    @pytest.mark.personal_tests
+    def test_guest_can_add_review(self, browser):
+        # Незарегистрированный пользователь может оставить отзыв
+        pass
+
+    @pytest.mark.personal_tests
+    def test_guest_cant_add_to_wishlist(self, browser):
+        # Кнопка "Добавить к списку желаемого" недоступна
+        pass
+
+    @pytest.mark.personal_tests
+    @pytest.mark.parametrize('search_string',
+                             ["python", "handbook",])
+    def test_guest_can_search(self, browser, search_string):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.search_for_product(search_string)
+        page.should_be_search_results(search_string)
+
+    @pytest.mark.personal_tests
+    def test_no_search_results(self, browser, search_string="azaza"):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.search_for_product(search_string)
+        page.should_not_be_search_results()
+
+
+# class TestPagination():
+#     @pytest.fixture(scope="function", autouse=True)
+#     def count_pages(self, browser):
+#         # посчитать количество страниц по количеству продуктов
+#
+#         pass
+#     @pytest.mark.personal_tests
+#     def test_pagination(self, browser):
+#         pass
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+        register_page = LoginPage(browser, link)
+        register_page.open()
+        # регистрация нового пользователя
+        email = f'{uuid.uuid4().hex}@mail.org'
+        password = 'Nots0simplepassWord'
+        register_page.register_new_user(email, password)
+        register_page.should_see_registration_success_message()
+        register_page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_be_success_message()
+        page.should_change_price()
+
+    @pytest.mark.xfail
+    def test_user_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.should_not_be_success_message()
+
+    def test_user_cant_see_success_message_before_purchase(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.personal_tests
+    def test_user_can_add_review(self, browser):
+        # Зарегистрированный пользователь может оставить отзыв
+        pass
+
+    @pytest.mark.personal_tests
+    def test_user_can_add_to_wishlist(self, browser):
+        # Кнопка "Добавить к списку желаемого" доступна
+        pass
